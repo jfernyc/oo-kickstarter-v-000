b37efa7db82c56772b1c722ff15fb8b9a9398c49
J Fern <jfern20@gmail.com>
2018-10-19 04:41:07 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,6 @@
 class Backer
   
- attr_accessor :name, :backed_projects 
+ attr_accessor :name, :back_projects 
   
   def initialize(name)
     @name = name 
@@ -9,7 +9,7 @@ class Backer
   end 
   
   
-   def backed_projects(project)
+   def back_projects(project)
       @backed_projects << project 
      
      
